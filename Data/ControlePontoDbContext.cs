c55884bf4e57ee7744356e4f42c43349303a6380
Lucas Santos <lucas.santos@sp.senai.br>
2024-03-13 20:09:13 -0300

    Arquivo de configuração

--- Data/ControlePontoDbContext.cs
+++ Data/ControlePontoDbContext.cs
@@ -1,6 +1,9 @@
 ﻿
 using ControlePonto.Modelos;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.IO;
 
 namespace ControlePonto.Data
 {
@@ -14,9 +17,20 @@ namespace ControlePonto.Data
         public DbSet<Cargo> Cargos { get; set; }
 
         
-        public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // Importa o nosso arquivo de configuração JSON
+            var builder = new ConfigurationBuilder()
+                            .AddJsonFile("appconfig.json", false, true);
+            
+            // Compila as configurações
+            IConfigurationRoot configuracao = builder.Build();
 
+            // Pega a string de conexão da configuracao
+            var stringDeConexao = configuracao.GetConnectionString("ConexaoPadrao");
+
+            // Adiciona a string a configuração do DbContext
+            optionsBuilder.UseMySql(stringDeConexao, ServerVersion.AutoDetect(stringDeConexao));
         }
     }
 }
