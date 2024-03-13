b901e150d40bb90d68ae90c65f0dcefbf3000716
Lucas Santos <lucas.santos@sp.senai.br>
2024-03-13 19:39:54 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ControlePontoDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using ControlePonto.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlePonto.Data
+{
+    internal class ControlePontoDbContext : DbContext
+    {
+        public ControlePontoDbContext(DbContextOptions<ControlePontoDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cargo> Cargos { get; set; }
+
+        
+        public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
